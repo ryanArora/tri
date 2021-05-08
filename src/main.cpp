fff7a044c08254521727b979fe16c7f80de43354
Ryan Arora <arora.ryan@gmail.com>
2021-05-08 11:37:06 -0400

    const

--- src/main.cpp
+++ src/main.cpp
@@ -87,7 +87,7 @@ int main(void)
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
 
-  float vertices[] = {
+  const float vertices[] = {
     -0.5f, -0.5f, 0.0f,
     0.5f, -0.5f, 0.0f,
     0.0f,  0.5f, 0.0f
